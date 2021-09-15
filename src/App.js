8052ccfd7170cee7ea6664aa67341908afa66b59
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-09-15 17:18:27 -0300

    get word and display typing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+
+import wordList from './resources/words.json';
+
+const maxTypedKeys = 30;
+
+const getWord = () => {
+  const index = Math.floor(Math.random() * wordList.length);
+  const word = wordList[index];
+
+  return word.toLowerCase();
+};
+
+const isValidKey = (key, word) => {
+  if (!word) return false;
+
+  const result = word.split('').includes(key);
+  return result;
+};
+
+const Word = ({ word, validKeys }) => {
+  if (!word) return null;
+
+  const joinedKeys = validKeys.join('');
+  const matched = word.slice(0, joinedKeys.length);
+  const remainder = word.slice(joinedKeys.length);
+
+  return (
+    <>
+      <span className="matched">{matched}</span>
+      <span className="remainder">{remainder}</span>
+    </>
+  );
+};
 
 const App = () => {
+  const [typedKeys, setTypedKeys] = useState([]);
+  const [word, setWord] = useState('');
+  const [validKeys, setValidKeys] = useState([]);
+
+  useEffect(() => {
+    setWord(getWord());
+  }, []);
+
+  const handleKeyDown = (event) => {
+    event.preventDefault();
+
+    const { key } = event;
+
+    setTypedKeys((prevTypedKeys) =>
+      [...prevTypedKeys, key].slice(maxTypedKeys * -1)
+    );
+
+    if (isValidKey(key, word)) {
+      setValidKeys((prev) => {
+        const isValidLength = prev.length <= word.length;
+        const isNextChar = isValidLength && word[prev.length] === key;
+
+        return isNextChar ? [...prev, key] : prev;
+      });
+    }
+  };
+
   return (
-    <div className="container">
+    <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
       <div className="valid-keys">
-        <span className="matched"></span>
-        <span className="remainder"></span>
+        <Word word={word} validKeys={validKeys} />
       </div>
-      <div className="typed-keys"></div>
+      <div className="typed-keys">{typedKeys && typedKeys.join(' ')}</div>
       <div className="completed-words">
         <ol>
           <li></li>
